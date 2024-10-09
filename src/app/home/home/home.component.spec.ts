258afc6fc54da7fc2a012a9f22334c38739b21b8
Jhorman Cadena Valero <jhor@MacBook-Air-de-Jhorman.local>
2024-10-09 11:23:19 +0200

    se realiza test unitario para el componente Home y se realiza comprobación de test

--- src/app/home/home/home.component.spec.ts
+++ src/app/home/home/home.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +8,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [HomeComponent, ReactiveFormsModule]
     })
     .compileComponents();
 
@@ -17,7 +17,9 @@ describe('HomeComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create total correct cost', () => {
+    component.formularioEjemplo.setValue({seo:true,ads:true,web:true});
+    component.actualizarCosteWeb(200)
+    expect(component.totalCoste).toBeTruthy();
   });
 });
--- src/app/panel/panel/panel.component.ts
+++ src/app/panel/panel/panel.component.ts
@@ -58,6 +58,5 @@ ngOnInit(): void {
   })
 }
 
-
 }
 
